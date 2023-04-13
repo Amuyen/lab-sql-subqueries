fdc1fdeb88c725df84aa08ad418904e2d2962782
Amuyen <pablo.nebel@gmail.com>
2023-04-13 15:27:33 +0100

    upload lab

--- lab-sql-subqueries.sql
+++ lab-sql-subqueries.sql
@@ -11,6 +11,7 @@ select title, length from film f
 WHERE length > (select avg(length) as average from film);
 
 
+-- Use subqueries to display all actors who appear in the film Alone Trip.
 SELECT concat(first_name,' ',last_name) as 'Actors in "Alone Trip"' from actor
 WHERE actor_id IN (
 	SELECT actor_id FROM film_actor
@@ -53,11 +54,33 @@ WHERE co.country="Canada";
 -- Most prolific actor is defined as the actor that has acted in the most number of films
 -- First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
 
-SELECT f.Title FROM Film f
+SELECT f.Title as 'Movies by most Prolific Actor' FROM Film f
 join film_actor fa
 on f.film_id=fa.film_id
 join (SELECT actor_id, count(*) as count from film_actor 
     GROUP BY actor_id order by count desc limit 1) a
-on fa.actor_id=a.actor_id
+on fa.actor_id=a.actor_id;
     
-    
+
+-- Films rented by most profitable customer. 
+-- You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
+
+SELECT f.Title as 'Movies rented by most profitable customer' 	FROM Film f
+JOIN inventory i
+ON f.film_id=i.film_id
+JOIN rental r
+ON i.inventory_id=r.inventory_id
+JOIN (SELECT customer_id, sum(amount) as sum from payment group by customer_id order by sum desc limit 1) p
+ON r.customer_id=p.customer_id;
+
+-- Customers who spent more than the average payments.
+
+select concat(first_name,' ',last_name) as Customer from Customer c
+join (
+	SELECT sum(amount) as sum, customer_id FROM payment
+	GROUP BY customer_id 
+	HAVING sum > (
+		SELECT avg(sum) FROM (
+			SELECT sum(amount) as sum, customer_id from payment 
+            GROUP BY customer_id) a)) p
+ON c.customer_id=p.customer_id
